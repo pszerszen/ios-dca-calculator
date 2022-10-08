6f281c77b1a259d8eb694eef76148697ba93b551
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2022-10-08 18:37:21 +0200

    introducing ApiService

---
+++ ios-dca-calculator/service/ApiService.swift
@@ -0,0 +1,16 @@
+//
+//  ApiService.swift
+//  ios-dca-calculator
+//
+//  Created by Piotr Szerszeń on 08/10/2022.
+//
+
+import Foundation
+
+struct ApiService {
+    
+    let API_KEYS = ["X5IF5LVKRU0XGVJG", "BNKMFIYE8XBT3H4S", "MXDG4Y0IIAMUMUZ5"]
+    
+    
+    
+}
