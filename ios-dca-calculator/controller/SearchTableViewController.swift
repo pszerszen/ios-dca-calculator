f11ab733d4f40da19b53ae5cd95b16a54acb274d
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2022-10-08 18:24:45 +0200

    TableViewCell

--- ios-dca-calculator/controller/SearchTableViewController.swift
+++ ios-dca-calculator/controller/SearchTableViewController.swift
@@ -27,7 +27,15 @@ class SearchTableViewController: UITableViewController {
     private func setupNavigationBar() {
         navigationItem.searchController = searchController
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        return cell
+    }
 }
 
 extension SearchTableViewController: UISearchResultsUpdating, UISearchControllerDelegate {
---
+++ ios-dca-calculator/view/SearchTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SearchTableViewCell.swift
+//  ios-dca-calculator
+//
+//  Created by Piotr Szerszeń on 08/10/2022.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var assetNameLabel: UILabel!
+    @IBOutlet weak var assetSymbolLabel: UILabel!
+    @IBOutlet weak var assetTypeLabel: UILabel!
+    
+}
